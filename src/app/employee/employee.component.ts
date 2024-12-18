c3fce62ef78c12728f4d07d39ef55ba33e43b970
Nithyasri-2516 <nithyasri1625@gmail.com>
2024-12-18 15:50:52 +0530

    add files

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,18 +8,19 @@ styleUrls: ['./employee.component.css']
 export class EmployeeComponent {
 employees: Employee[] = [];
 newEmployee: Employee = { id: 0, name: '', salary: 0, fieldOfWork: '' };
+
 addEmployee() {
 
-if (this.newEmployee.name && this.newEmployee.salary > 0 &&
-this.newEmployee.fieldOfWork) {
+if (this.newEmployee.id && this.newEmployee.name && this.newEmployee.salary > 0 && this.newEmployee.fieldOfWork) {
 this.employees.push({ ...this.newEmployee });
 this.resetForm();
 }
 }
+
 deleteEmployee(id: number) {
-this.employees = this.employees.filter(employee => employee.id !==
-id);
+this.employees = this.employees.filter(employee => employee.id !==id);
 }
+
 resetForm() {
 this.newEmployee = { id: 0, name: '', salary: 0, fieldOfWork: '' };
 }
